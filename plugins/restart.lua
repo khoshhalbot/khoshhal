144bbaac3e87e0256a0868446af2b33614274b8f
khoshhalbot <parsah460@yahoo.com>
2016-06-01 22:50:01 +0430

    Create restart.lua

---
+++ plugins/restart.lua
@@ -0,0 +1,17 @@
+
+
+
+function run(msg, matches)
+if not is_sudo(msg) then
+return 
+end
+text = io.popen(" ./launch.sh "):read('*all')
+  return text
+end
+return {
+  patterns = {
+    '^[#/!]launch$'
+  },
+  run = run,
+  moderated = true
+}
